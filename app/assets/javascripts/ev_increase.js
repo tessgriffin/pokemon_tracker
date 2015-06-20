013add32eac6054e97c4654f13a8995810d8e99d
Tess Griffin <tessjgriffin@gmail.com>
2015-06-20 11:48:34 -0600

    Implemented JQuery for horde battles. Changed layout of pokemon show page.
    Implemented JQuery for horde battles to have that working before tring to implement Angular. Changed
    The layout of the show page to be more compact because before you had to scroll down quite a bit for
    everything.

--- app/assets/javascripts/ev_increase.js
+++ app/assets/javascripts/ev_increase.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+  addEv();
+  addHordeEv();
+})
+
+var addEv = function(){
   $(".ev_inc").on("click", function(){
     var stat = $(this).attr('data-stat')
     var val  = $(this).attr('data-value')
@@ -10,8 +15,27 @@ $(document).ready(function(){
       url: '/pokemon/' + id + '/add_ev',
       data: postParams,
       success: function(pokemon) {
-        $("#" + stat + "_value").text(pokemon.evs[stat]);
+        $("." + stat + "_value").text(pokemon.evs[stat]);
       }
     })
   })
-})
+}
+
+var addHordeEv = function(){
+  $("#log").on("click", function(){
+    var evId = $("#species").val();
+    var amount = $("#amount").val();
+    var url  = document.URL.split('/')
+    var id   = url[url.length - 1]
+    var postParams = { pokemonId: id, pokemon: {evs: evId, amount: amount}}
+    $.ajax({
+      type: 'POST',
+      url: '/pokemon/' + id + '/increase_evs',
+      data: postParams,
+      success: function(response) {
+        stat = response.stat
+        $("." + stat + "_value").text(response.pokemon.evs[stat]);
+      }
+    })
+  })
+}
